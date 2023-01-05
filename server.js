2ccf18250a77dffe5165e1e1a9625ea231c2e35d
Charlie <charlieryancurry@gmail.com>
2023-01-05 14:47:38 -0800

    set up routes folder

--- server.js
+++ server.js
@@ -36,49 +36,26 @@ app.use(express.static('public'));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-// const userApiRoutes = require('./routes/users-api');
-// const widgetApiRoutes = require('./routes/widgets-api');
-// const usersRoutes = require('./routes/users');
-
+const userApiRoutes = require('./routes/users-api');
+const widgetApiRoutes = require('./routes/widgets-api');
+const usersRoutes = require('./routes/users');
+const loginRoutes = require('./routes/login-route');
+const registerRoutes = require('./routes/register-route');
+const indexRoutes = require('./routes/index');
 // // Mount all resource routes
 // // Note: Feel free to replace the example routes below with your own
 // // Note: Endpoints that return data (eg. JSON) usually start with `/api`
-// app.use('/api/users', userApiRoutes);
-// app.use('/api/widgets', widgetApiRoutes);
-// app.use('/users', usersRoutes);
+app.use('/api/users', userApiRoutes);
+app.use('/api/widgets', widgetApiRoutes);
+app.use('/users', usersRoutes);
+app.use('/login', loginRoutes);
+app.use('/register', registerRoutes);
+app.use('/', indexRoutes);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
-
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.get('/login', (req, res) => {
-  res.render('login');
-});
-
-app.get('/register', (req, res) => {
-  res.render('register');
-});
-
-app.post('/login', (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  console.log(email);
-  console.log(password);
-  res.render('index')
-});
-
-app.post('/register', (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  console.log(email);
-  console.log(password);
-  res.render('index')
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
