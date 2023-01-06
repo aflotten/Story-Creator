3e0ed20f49904973c9d442e3be978c5bd636cf32
Charlie <charlieryancurry@gmail.com>
2023-01-06 15:07:16 -0800

    make login only work with a users real credentials

--- routes/login-route.js
+++ routes/login-route.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const userQueries = require('../db/queries/users');
 
 // Get method for displaying login
 router.get('/', (req, res) => {
@@ -9,10 +11,20 @@ router.get('/', (req, res) => {
 // Post method for sending loggin credentials
 router.post('/', (req, res) => {
   const email = req.body.email;
-  const password = req.body.password;
-  console.log(email);
-  console.log(password);
-  res.render('index');
+  const password = req.body.password
+  userQueries.getUserByEmail(email)
+  .then(user => {
+    if(user === undefined) {
+      res.send({error: "error logging in user"})
+      return;
+    } else if (bcrypt.compareSync(password, user.password) !== true) {
+      res.send({error: "error logging in user"})
+    } else {
+      req.session.userId = user.id;
+      res.send("Successfully logged in user!")
+    }
+  })
+  .catch(error => res.send(error))
 });
 
 module.exports = router;
