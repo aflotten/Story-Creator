adb303f3de645de38a68aab6d3c0de4122daeabd
Charlie <charlieryancurry@gmail.com>
2023-01-06 19:33:32 -0800

    clean up seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,6 +1,4 @@
 -- Users table seeds here (Example)
-
--- const bcrypt = require('bcryptjs');
 INSERT INTO users (name, email, password) VALUES ('Alice', 'alice5@gmail.com',  '$2a$10$xeQYtjQqWtK0lNp9TUy23exy4ShrrXTe/0BizbpVHRP94nBLviwBK');
 INSERT INTO users (name, email, password) VALUES ('Jimmy', 'jimmywheels@gmail.com', ' $2a$10$xeQYtjQqWtK0lNp9TUy23exy4ShrrXTe/0BizbpVHRP94nBLviwBK');
 INSERT INTO users (name, email, password) VALUES ('Maddison', 'maddycoolhat@gmail.com', ' $2a$10$xeQYtjQqWtK0lNp9TUy23exy4ShrrXTe/0BizbpVHRP94nBLviwBK');
