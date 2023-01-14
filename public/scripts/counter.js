d54e950488a0c4754b95320b8eb2b39cb4c4d41d
Gozi1 <snwabiani@gmail.com>
2023-01-14 08:03:34 +0000

    additions post div working as intended now hopefully

--- public/scripts/counter.js
+++ public/scripts/counter.js
@@ -5,18 +5,17 @@ $(document).ready(function() {
 
   /*checks charater count present in the input field
   if over 1000 color changes red */
-  additionInput.on('input', () => {
-    const counter = additionInput.closest('form').children('div').children('output');
+  $('main').on('input',additionInput, (e) => {
 
-    const addition = additionInput.val();
-    const newLength = 1000 - addition.length;
-
+    const counter = e.target.closest('form').children[1].children[0];
+    const addition = e.target.value.length;
+    const newLength = 1000 - addition;
     if (newLength >= 0) {
 
-      counter.removeClass('over-counter');
+      $(counter).removeClass('over-counter');
     } else {
-      counter.addClass('over-counter');
+      $(counter).addClass('over-counter');
     }
-    counter.val(newLength);
+    counter.value = newLength;
   });
 });
